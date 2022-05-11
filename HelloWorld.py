c67979376c9b71f31efb2b7b2e6e38c583f35247
Ryan Stevens <restevens52@gmail.com>
2022-05-11 14:56:17 -0700

    made Hello World, function prints to console.

---
+++ HelloWorld.py
@@ -0,0 +1,7 @@
+# Ryan Stevens
+# restevens52@gmail.com
+#
+# The classic introduction to a language will bring me good luck in my coding adventures!
+# prints Hello World! to the console.
+
+print("Hello World!")
