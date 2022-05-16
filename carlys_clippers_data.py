aee673b44b2f5bb7227e92563806d88f980e36ed
Ryan Stevens <restevens52@gmail.com>
2022-05-16 15:18:44 -0700

    ported the carly's clippers from codecademy

---
+++ carlys_clippers_data.py
@@ -0,0 +1,40 @@
+# Ryan Stevens
+# restevens52@gmail.com
+#
+# go through the lists of data that have been collected in the past couple of weeks, calculating some
+# important metrics that Carly can use to plan out the operation of the business
+
+
+hairstyles = ["bouffant", "pixie", "dreadlocks", "crew", "bowl", "bob", "mohawk", "flattop"]
+
+prices = [30, 25, 40, 20, 20, 35, 50, 35]
+
+last_week = [2, 3, 5, 8, 4, 4, 6, 2]
+
+total_price = 0
+
+for price in prices:
+    total_price += price
+
+average_price = total_price/len(prices)
+
+print("Average Haircut Price:", average_price)
+
+new_prices = [price-5 for price in prices]
+
+print(new_prices)
+
+total_revenue = 0
+
+i = 0
+for i in range(len(hairstyles)):
+    total_revenue += prices[i]*last_week[i]
+print("Total Revenue:",total_revenue)
+
+average_daily_revenue = total_revenue/7
+
+print(average_daily_revenue)
+
+cuts_under_30 = [hairstyles[i] for i in range(len(new_prices)) if new_prices[i] < 30]
+
+print(cuts_under_30)
