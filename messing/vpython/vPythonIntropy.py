3c4d532b5880174c5f4a4a0aa541ab637cc95a69
Ryan Stevens <restevens52@gmail.com>
2023-03-16 20:16:35 -0700

    created a vitr env with vpython, experimented

---
+++ messing/vpython/vPythonIntropy.py
@@ -0,0 +1,11 @@
+from vpython import *
+
+#ball = sphere(color=color.red)
+circle = shapes.circle(radius=10)
+circle_path = [vector(0,0,0),vector(0,0,1),vector(0,1,1)]
+disk = extrusion(path = circle_path, shape = circle)
+
+sleep(5)
+#ball.color=color.blue
+while True:
+    pass
